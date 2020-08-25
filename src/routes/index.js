a3aa0a90be7961a88364851949fd5ade8e001520
AnasAito <anasaitaomar1999@gmail.com>
2020-08-25 15:20:01 +0100

    add testing view

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Tests from "screens/Tests";
 import Blocks from "screens/Blocks";
 import Grade from "screens/Grade";
 import Report from "screens/Report";
+import Testing from "screens/Testing";
 
 var indexRoutes = [
   {
@@ -28,6 +29,7 @@ var indexRoutes = [
   { path: "/blocks", name: "Blocks", component: Blocks, layout: "/admin" },
   { path: "/grade", name: "Grade", component: Grade, layout: "/admin" },
   { path: "/report", name: "Report", component: Report, layout: "/admin" },
+  { path: "/testing", name: "testing ", component: Testing, layout: "/admin" },
 ];
 
 export default indexRoutes;
